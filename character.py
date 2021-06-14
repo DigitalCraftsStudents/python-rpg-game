89b980e74111bf9c3470076e69254d001cd27377
Karen Seunsom <karenseuns@gmail.com>
2021-06-14 15:36:48 -0500

    second menu option up and running

--- character.py
+++ character.py
@@ -1,12 +1,12 @@
 class Character:
-    def __init__(self, name, health=10, power=5):
+    def __init__(self, name, health=100, power=5):
         self.name = name
         self.health = health
         self.power = power
 
     def attack(self, other_person):
         other_person.health -= self.power
-        print(f'{self.name} does {self.power} damage to {other_person.name}')
+        print(f'\n{self.name} does {self.power} damage to {other_person.name}')
         
     def is_alive(self):
         self.health > 0
